8625d991c511699246a0bef3efdcc367ab397f79
Arnav Patel <patelarnavm+git@gmail.com>
2022-10-12 20:27:07 -0400

    fixed dark mode transition

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-// 
-// 
-// 
+//
+//
+//
 
 function toggleContrast() {
   document.body.classList.toggle("dark-mode");
+  document.querySelector(".toggle-contrast").classList.toggle("fa-sun");
+  document.querySelector(".toggle-contrast").classList.toggle("fa-moon");
 }
 
 function contact(event) {
   event.preventDefault();
-  const loading = document.querySelector('.contact-loading');
-  const success = document.querySelector('.contact-success');
+  const loading = document.querySelector(".contact-loading");
+  const success = document.querySelector(".contact-success");
   loading.classList += " contact-visible";
   emailjs
     .sendForm(
-      'service_2huaien',
-      'template_37x87en',
+      "service_2huaien",
+      "template_37x87en",
       event.target,
-      '2hvV9SMMYfDlNPLDE'
-    ).then(() => {
+      "2hvV9SMMYfDlNPLDE"
+    )
+    .then(() => {
       loading.classList.remove("contact-visible");
       success.classList += " contact-visible";
-    }).catch(() => {
-      loading.classList.remove("contact-visible");
-      alert("The email service is temporarily unavailable. Please contact me directly on patelarnavm@gmail.com");
     })
+    .catch(() => {
+      loading.classList.remove("contact-visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on patelarnavm@gmail.com"
+      );
+    });
 }
 
 function openInfo() {
-  document.querySelector('body').classList.add('info-open');
-  document.querySelector('.info').classList.add('info-open');
-  document.querySelector('.mail-link').classList.add('back');
+  document.querySelector("body").classList.add("info-open");
+  document.querySelector(".info").classList.add("info-open");
+  document.querySelector(".mail-link").classList.add("back");
 }
 
 function closeInfo() {
-  document.querySelector('.info').classList.remove('info-open');
-  document.querySelector('body').classList.remove('info-open');
-  document.querySelector('.mail-link').classList.remove('back');
-  document.querySelector('.contact-success').classList.remove('contact-visible')
-  document.querySelector('.contact-loading').classList.remove('contact-visible')
+  document.querySelector(".info").classList.remove("info-open");
+  document.querySelector("body").classList.remove("info-open");
+  document.querySelector(".mail-link").classList.remove("back");
+  document
+    .querySelector(".contact-success")
+    .classList.remove("contact-visible");
+  document
+    .querySelector(".contact-loading")
+    .classList.remove("contact-visible");
 }
